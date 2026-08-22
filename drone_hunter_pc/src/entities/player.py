ad9cec7034bc35aa814107344513b1fd838092d0
DESKTOP-J523A0T\DELL <abdoelhawey4@gmail.com>
2026-08-22 17:59:41 +0300

    Upgrade Heavy Plasma cartoon visuals and boost mouse flight acceleration and drag responsiveness

--- drone_hunter_pc/src/entities/player.py
+++ drone_hunter_pc/src/entities/player.py
@@ -68,8 +68,8 @@ class Player(pygame.sprite.Sprite):
         # Position & Kinematic Flight Physics (Fast-Paced, Highly Responsive)
         self.pos = pygame.Vector2(pos)
         self.velocity = pygame.Vector2(0, 0)
-        self.acceleration = 5400.0
-        self.drag = 6.0
+        self.acceleration = 6400.0
+        self.drag = 5.0
         self.max_speed = HORIZONTAL_SPEED
         self.is_accelerating = False
         
